1bff7058dc215da9fb09fff1c281f61aa8c47c9d
codecafe <ogechiokachi4real@gmail.com>
2025-07-11 15:23:59 +0100

    welcome message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-700">
       <h1 className="text-white text-4xl font-bold">
-        Welcome to Debiekreatives Portfolio!
+        Welcome to Debiekreatives World!
       </h1>
     </div>
   );
