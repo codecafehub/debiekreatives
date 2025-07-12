609da1ad0bcf784595b5cd03316fce73f5469cde
codecafe <ogechiokachi4real@gmail.com>
2025-07-12 06:45:47 +0100

    link

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/*
+      Add the basename prop here.
+      process.env.PUBLIC_URL will automatically resolve to "/debiekreatives"
+      based on the "homepage" field in your package.json.
+    */}
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
